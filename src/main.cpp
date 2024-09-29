0010fe746946cb2a179ec3cb3878a616e3341b43
Binh Chau <binhchau@beetechsolution.vn>
2024-09-29 17:10:28 +0700

    Control builtin led by pressing button on web page

--- src/main.cpp
+++ src/main.cpp
@@ -3,61 +3,29 @@
 #include <WebServer.h> // standard library
 #include <web_page.h>
 
+/* CONSTANTS ===============================*/
 #define TESTING_NEW_LIB 1
+//#define LED_BUILTIN 2
 
-// Replace with your network credentials
+/* WIFI CREDENTIALS*/
 const char *ssid = "BINHPHAM";
 const char *password = "8534779a";
 
-// Set web server port number to 80
+/* VARIABLES ===============================*/
 WebServer server(80);
-#ifndef TESTING_NEW_LIB
-WiFiClient client;
-#endif
+char XML[2048];
 
-// Variable to store the HTTP request
-String header;
+/* I/O STATES ===============================*/
+bool led_builtin_state = false;
 
-// Auxiliar variables to store the current output state
-String output26State = "off";
-String output27State = "off";
-
-// Assign output variables to GPIO pins
-const int output26 = 26;
-const int output27 = 27;
-
-// Set your Static IP address
-IPAddress local_IP(192, 168, 0, 198);
-IPAddress gateway(192, 168, 0, 1);
-IPAddress subnet(255, 255, 255, 255);
-IPAddress primaryDNS(0, 0, 0, 0);   // optional
-IPAddress secondaryDNS(0, 0, 0, 0); // optional
-
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0;
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
-
-
-void SendWebsite()
-{
-
-  Serial.println("sending web page");
-  server.send(200, "text/html", PAGE_MAIN);
-}
-
+void SendWebsite();
+void SendXML();
+void turnon_led_builtin();
 
 void setup()
 {
   Serial.begin(115200);
-  // Initialize the output variables as outputs
-  pinMode(output26, OUTPUT);
-  pinMode(output27, OUTPUT);
-  // Set outputs to LOW
-  digitalWrite(output26, LOW);
-  digitalWrite(output27, LOW);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
@@ -68,8 +36,8 @@ void setup()
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
     Serial.print(".");
+    delay(500);
   }
   // Print local IP address and start web server
   Serial.println("");
@@ -78,6 +46,10 @@ void setup()
   Serial.println(WiFi.localIP());
 
   server.on("/", SendWebsite);
+  server.on("/xml", SendXML);
+  server.on("/BUTTON_0", turnon_led_builtin);
+  // server.on("/xml", SendXML);
+
   server.begin();
 }
 
@@ -86,127 +58,65 @@ void loop()
   server.handleClient();
 }
 
-
-#ifndef TESTING_NEW_LIB
-void loop()
+void SendWebsite()
 {
-  client = server.available();
-  if (client)
-  { // If a new client connects,
-    currentTime = millis();
-    previousTime = currentTime;
-    Serial.println("New Client."); // print a message out in the serial port
-    String currentLine = "";       // make a String to hold incoming data from the client
-    while (client.connected() && currentTime - previousTime <= timeoutTime)
-    { // loop while the client's connected
-      currentTime = millis();
-      if (client.available() && 2 == 1)
-      {                         // if there's bytes to read from the client,
-        char c = client.read(); // read a byte, then
-        Serial.write(c);        // print it out the serial monitor
-        header += c;
-        if (c == '\n')
-        { // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0)
-          {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
+  Serial.println("sending web page");
+  server.send(200, "text/html", PAGE_MAIN);
+}
 
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /26/on") >= 0)
-            {
-              Serial.println("GPIO 26 on");
-              output26State = "on";
-              digitalWrite(output26, HIGH);
-            }
-            else if (header.indexOf("GET /26/off") >= 0)
-            {
-              Serial.println("GPIO 26 off");
-              output26State = "off";
-              digitalWrite(output26, LOW);
-            }
-            else if (header.indexOf("GET /27/on") >= 0)
-            {
-              Serial.println("GPIO 27 on");
-              output27State = "on";
-              digitalWrite(output27, HIGH);
-            }
-            else if (header.indexOf("GET /27/off") >= 0)
-            {
-              Serial.println("GPIO 27 off");
-              output27State = "off";
-              digitalWrite(output27, LOW);
-            }
+void turnon_led_builtin()
+{
+  // switch state
+  led_builtin_state = !led_builtin_state;
+  digitalWrite(LED_BUILTIN, led_builtin_state);
 
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #555555;}</style></head>");
-
-            // Web Page Heading
-            client.println("<body><h1>ESP32 Web Server</h1>");
-
-            // Display current state, and ON/OFF buttons for GPIO 26
-            client.println("<p>GPIO 26 - State " + output26State + "</p>");
-            // If the output26State is off, it displays the ON button
-            if (output26State == "off")
-            {
-              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
-            }
-            else
-            {
-              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-
-            // Display current state, and ON/OFF buttons for GPIO 27
-            client.println("<p>GPIO 27 - State " + output27State + "</p>");
-            // If the output27State is off, it displays the ON button
-            if (output27State == "off")
-            {
-              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
-            }
-            else
-            {
-              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-            client.println("</body></html>");
-
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          }
-          else
-          { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        }
-        else if (c != '\r')
-        {                   // if you got anything else but a carriage return character,
-          currentLine += c; // add it to the end of the currentLine
-        }
-      }
-      // server.send(200, "text/html", PAGE_MAIN);
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
+  if (led_builtin_state)
+  {
+    server.send(200, "text/plain", "1"); // Send web page
+  }
+  else
+  {
+    server.send(200, "text/plain", "0"); // Send web page
   }
 }
 
-#endif
+
+void SendXML() {
+  strcpy(XML, "<?xml version = '1.0'?>\n<Data>\n");
+
+  // // send bitsA0
+  // sprintf(buf, "<B0>%d</B0>\n", BitsA0);
+  // strcat(XML, buf);
+  // // send Volts0
+  // sprintf(buf, "<V0>%d.%d</V0>\n", (int) (VoltsA0), abs((int) (VoltsA0 * 10)  - ((int) (VoltsA0) * 10)));
+  // strcat(XML, buf);
+
+  // // send bits1
+  // sprintf(buf, "<B1>%d</B1>\n", BitsA1);
+  // strcat(XML, buf);
+  // // send Volts1
+  // sprintf(buf, "<V1>%d.%d</V1>\n", (int) (VoltsA1), abs((int) (VoltsA1 * 10)  - ((int) (VoltsA1) * 10)));
+  // strcat(XML, buf);
+
+  // show led0 status
+  if (LED_BUILTIN) {
+    strcat(XML, "<LED>1</LED>\n");
+  }
+  else {
+    strcat(XML, "<LED>0</LED>\n");
+  }
+
+  // if (SomeOutput) {
+  //   strcat(XML, "<SWITCH>1</SWITCH>\n");
+  // }
+  // else {
+  //   strcat(XML, "<SWITCH>0</SWITCH>\n");
+  // }
+
+  strcat(XML, "</Data>\n");
+
+  Serial.println(XML);
+
+  /* Send XML to server with value of 200ms*/
+  server.send(200, "text/xml", XML);
+}
